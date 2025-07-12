cbc7b9d30d18cb179ce6c5da667f29e73b964c97
jp15310 <jpfortin@xhpsi.com>
2025-07-12 16:17:29 -0400

    Update CDK pipeline

---
+++ cdk_aws_setup/cdk_aws_setup_stage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""Module providing function ...."""
+
+import logging
+import aws_cdk as cdk
+# from constructs import Construct
+from cdk_aws_setup.cdk_aws_setup_stack import CdkAwsSetupStack
+
+
+class CdkAwsSetupStage(cdk.Stage):
+    def __init__(self, scope, construct_id, env=None, prefix=None, **kwargs, ):
+        super().__init__(scope, construct_id, env=env, **kwargs)
+
+        # Setup logging
+        logging.basicConfig(
+            format='%(asctime)s [%(levelname)5s] %(message)s',
+            datefmt='%Y-%m-%dT%H:%M:%S',
+            level=logging.NOTSET
+        )
+
+        msg = f"NetworkPipelineStage: CDK_DEFAULT_ACCOUNT: {env.account} - CDK_DEFAULT_REGION: {env.region} - PREFIX: {prefix}"
+        logging.info(msg)
+
+        CdkAwsSetupStack(self, 'hpsi-CdkAwsSetupStack', env, prefix)
--- tests/unit/test_cdk_aws_setup_stack.py
+++ tests/unit/test_cdk_aws_setup_stack.py
@@ -1,15 +1,14 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
-
-from cdk_aws_setup.cdk_aws_setup_stack import CdkAwsSetupStack
+# import aws_cdk as core
+# import aws_cdk.assertions as assertions
+# from cdk_aws_setup.cdk_aws_setup_stack import CdkAwsSetupStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cdk_aws_setup/cdk_aws_setup_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = CdkAwsSetupStack(app, "cdk-aws-setup")
-    template = assertions.Template.from_stack(stack)
+# def test_sqs_queue_created():
+# app = core.App()
+# stack = CdkAwsSetupStack(app, "cdk-aws-setup")
+# template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+# template.has_resource_properties("AWS::SQS::Queue", {
+#    "VisibilityTimeout": 300
+# })
